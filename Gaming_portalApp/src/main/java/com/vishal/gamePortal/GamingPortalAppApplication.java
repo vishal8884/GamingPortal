4aaf210558bf82c0e7afece8c0411bad0f5104da
vishal <vishalambekar459@gmail.com>
2021-07-11 11:05:01 +0530

    First Commit

---
+++ Gaming_portalApp/src/main/java/com/vishal/gamePortal/GamingPortalAppApplication.java
@@ -0,0 +1,13 @@
+package com.vishal.gamePortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingPortalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingPortalAppApplication.class, args);
+	}
+
+}
---
+++ Gaming_portalApp/src/test/java/com/vishal/gamePortal/GamingPortalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishal.gamePortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingPortalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

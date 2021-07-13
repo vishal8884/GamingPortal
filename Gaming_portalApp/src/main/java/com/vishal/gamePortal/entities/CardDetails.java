84c809e2b4612c5b3d7f60e5e8891e8210aa8db9
vishal <vishalambekar459@gmail.com>
2021-07-13 14:24:17 +0530

    Utility classes added

---
+++ Gaming_portalApp/src/main/java/com/vishal/gamePortal/entities/CardDetails.java
@@ -0,0 +1,59 @@
+package com.vishal.gamePortal.entities;
+
+public class CardDetails {
+
+	private long id;
+	private String cardNumber;
+	private String CVV;
+	private String expiryDate;
+	
+	public CardDetails()
+	{
+		
+	}
+	
+	
+	
+	public CardDetails(long id, String cardNumber, String cVV, String expiryDate) {
+		super();
+		this.id = id;
+		this.cardNumber = cardNumber;
+		this.CVV = cVV;
+		this.expiryDate = expiryDate;
+	}
+
+    
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getCardNumber() {
+		return cardNumber;
+	}
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	public String getCVV() {
+		return CVV;
+	}
+	public void setCVV(String cVV) {
+		CVV = cVV;
+	}
+	public String getExpiryDate() {
+		return expiryDate;
+	}
+	public void setExpiryDate(String expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+	@Override
+	public String toString() {
+		return "CardDetails [id=" + id + ", cardNumber=" + cardNumber + ", CVV=" + CVV + ", expiryDate=" + expiryDate
+				+ "]";
+	}
+	
+	
+	
+}
---
+++ Gaming_portalApp/src/main/java/com/vishal/gamePortal/entities/Game.java
@@ -0,0 +1,57 @@
+package com.vishal.gamePortal.entities;
+
+public class Game {
+	
+	
+	private String id;
+	private String name;
+	private String publisherName;
+	private int cost;
+	
+	
+	public Game()
+	{
+		
+	}
+	
+	
+	public Game(String id, String name, String publisherName, int cost) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.publisherName = publisherName;
+		this.cost = cost;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPublisherName() {
+		return publisherName;
+	}
+	public void setPublisherName(String publisherName) {
+		this.publisherName = publisherName;
+	}
+	public int getCost() {
+		return cost;
+	}
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+	@Override
+	public String toString() {
+		return "Game [id=" + id + ", name=" + name + ", publisherName=" + publisherName + ", cost=" + cost + "]";
+	}
+	
+	
+	
+
+}
